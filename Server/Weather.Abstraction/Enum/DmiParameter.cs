4058181942a93a09c3bda3316409b2cc717013fc
André Steenhoff Madsen <andre.madsen@specialisterne.dk>
2026-03-23 14:46:23 +0100

    fix: adds enums that where needed for previous commit

---
+++ Server/Weather.Abstraction/Enum/DmiParameter.cs
@@ -0,0 +1,10 @@
+﻿namespace Weather.Abstraction.Enum
+{
+    public enum DmiParameter
+    {
+        UNKNOWN = 0,
+        TEMP_DRY = 1,
+        HUMIDITY = 2,
+        PRESSURE = 3,
+    }
+}
---
+++ Server/Weather.Abstraction/Enum/Location.cs
@@ -0,0 +1,9 @@
+﻿namespace Weather.Abstraction.Enum
+{
+    public enum Location
+    {
+        UNKNOWN = 0,
+        INSIDE = 1,
+        OUTSIDE = 2,
+    }
+}
