8030a9737d380049c939588d0c3c4dff197cfece
André Steenhoff Madsen <andre.madsen@specialisterne.dk>
2026-03-23 14:45:01 +0100

    chore: adds persistence abstractions

---
+++ Server/Weather.Abstraction/Interfaces/Persistence/IComplexSearchable.cs
@@ -0,0 +1,12 @@
+namespace Weather.Abstraction.Interfaces.Persistence;
+
+// Todo: Consider at better name...
+
+/// <summary>
+/// Add Implementation for things that need a Complex search.
+/// </summary>
+/// <typeparam name="TSearchable"></typeparam>
+public interface IComplexSearchable<TSearchable> where TSearchable : class, ISearchable, new()
+{
+    TSearchable Searchable { get; set; }
+}
---
+++ Server/Weather.Abstraction/Interfaces/Persistence/IEntity.cs
@@ -0,0 +1,15 @@
+namespace Weather.Abstraction.Interfaces.Persistence
+{
+    public interface IEntity : ISearchable
+    {
+        /// <summary>
+        /// Exchange my type to fit with the type needed for concurrency control in the database.
+        /// Most databases can use a byte array for this purpose, but you can change it to something else if needed.
+        /// The important thing is that it should be able to detect changes to the entity and prevent concurrent updates from overwriting each other.
+        /// </summary>
+        uint Version { get; set; }
+
+        DateTime CreatedDateTime { get; set; }
+        DateTime UpdatedDateTime { get; set; }
+    }
+}
