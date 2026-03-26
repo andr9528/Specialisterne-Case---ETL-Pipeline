15ba58d92f70a2b3c88a0ef6a021d1fc5d49f14b
André Steenhoff Madsen <andre.madsen@specialisterne.dk>
2026-03-26 12:40:43 +0100

    chore: extends WeatherEntityFactory with methods to add the remaining entity types needed for upcoming tests.

--- Server/Weather.Tests/Core/WeatherEntityFactory.cs
+++ Server/Weather.Tests/Core/WeatherEntityFactory.cs
@@ -33,4 +33,63 @@ internal sealed class WeatherEntityFactory
 
         return entity;
     }
+
+    public async Task<Dmi> AddDmi(
+        int? id = null, Guid? dmiId = null, DmiParameter? parameterId = null, int stationId = 12345,
+        DateTime? observedAt = null, DateTime? pulledAt = null, double value = 12.3d)
+    {
+        var entity = new Dmi(id ?? 0)
+        {
+            DmiId = dmiId ?? Guid.NewGuid(),
+            ParameterId = parameterId ?? DmiParameter.TEMP_DRY,
+            StationId = stationId,
+            ObservedAt = observedAt ?? DateTime.UtcNow,
+            PulledAt = pulledAt ?? DateTime.UtcNow,
+            Value = value
+        };
+
+        await context.Dmi.AddAsync(entity);
+        await context.SaveChangesAsync();
+
+        return entity;
+    }
+
+    public async Task<Ds> AddDs(
+        int? id = null, Guid? readerId = null, Location? location = null, DateTime? observedAt = null,
+        DateTime? pulledAt = null, float temperature = 20.5f)
+    {
+        var entity = new Ds(id ?? 0)
+        {
+            ReaderId = readerId ?? Guid.NewGuid(),
+            Location = location ?? Location.INSIDE,
+            ObservedAt = observedAt ?? DateTime.UtcNow,
+            PulledAt = pulledAt ?? DateTime.UtcNow,
+            Temperature = temperature
+        };
+
+        await context.Ds.AddAsync(entity);
+        await context.SaveChangesAsync();
+
+        return entity;
+    }
+
+    public async Task<Scd> AddScd(
+        int? id = null, Guid? readerId = null, int carbonDioxide = 500, DateTime? observedAt = null,
+        DateTime? pulledAt = null, float humidity = 45.0f, float temperature = 20.5f)
+    {
+        var entity = new Scd(id ?? 0)
+        {
+            ReaderId = readerId ?? Guid.NewGuid(),
+            CarbonDioxide = carbonDioxide,
+            ObservedAt = observedAt ?? DateTime.UtcNow,
+            PulledAt = pulledAt ?? DateTime.UtcNow,
+            Humidity = humidity,
+            Temperature = temperature
+        };
+
+        await context.Scd.AddAsync(entity);
+        await context.SaveChangesAsync();
+
+        return entity;
+    }
 }
